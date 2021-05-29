37c23afb0ea54fa9ab1426bca6f5e3a65226e969
Alexcmyk <alex.kirts@gmail.com>
2021-05-29 15:57:17 -0600

    add close image and functionality

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,17 @@
 const mobileButton = document.querySelector('.mobile-nav');
+const mobileButtonImg = document.querySelector('.mobile-nav img');
 const mobilenav = document.querySelector('#mobile-nav');
+
+console.dir(mobileButtonImg);
 
 mobileButton.addEventListener('click', function () {
   if (mobilenav.classList.contains('open')) {
     mobilenav.classList.remove('open');
     document.body.style.overflow = 'auto';
+    mobileButtonImg.src = './images/icon-hamburger.svg';
   } else {
     mobilenav.classList.toggle('open');
     document.body.style.overflow = 'hidden';
+    mobileButtonImg.src = './images/icon-hamburger-closed.svg';
   }
 });
