d653f69395c7ec116f19a4f65103564a0c835b04
Alexcmyk <alex.kirts@gmail.com>
2021-05-30 09:07:15 -0600

    window resize function

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,7 @@
+const wd = document.defaultView;
 const mobileButton = document.querySelector('.mobile-nav');
 const mobileButtonImg = document.querySelector('.mobile-nav img');
 const mobilenav = document.querySelector('#mobile-nav');
-
-console.dir(mobileButtonImg);
 
 mobileButton.addEventListener('click', function () {
   if (mobilenav.classList.contains('open')) {
@@ -13,5 +12,11 @@ mobileButton.addEventListener('click', function () {
     mobilenav.classList.toggle('open');
     document.body.style.overflow = 'hidden';
     mobileButtonImg.src = './images/icon-hamburger-closed.svg';
+  }
+});
+
+wd.addEventListener('resize', function () {
+  if (window.innerWidth > 750) {
+    mobilenav.classList.remove('open');
   }
 });
